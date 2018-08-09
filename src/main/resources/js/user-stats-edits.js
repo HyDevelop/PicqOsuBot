a19d914773332151198b568fd93246000c68d94f
Hykilpikonna <admin@moecraft.cc>
2018-08-09 15:27:24 +0800

    [+] 创建stats编辑js

---
+++ src/main/resources/js/user-stats-edits.js
@@ -0,0 +1,3 @@
+// 用来检测的正则
+var regexChart = /<div class="profile-header-extra__rank-box">.*?pp<\/div>/g;
+var regexReplay = /<dl class="profile-stats__entry"><dt class="profile-stats__key">回放被观看次数<\/dt><dd class="profile-stats__value">.*?<\/dd><\/dl><div class="profile-stats__value profile-stats__value--score-ranks">.*?[0-9]<\/div><\/div><\/div>/g;
