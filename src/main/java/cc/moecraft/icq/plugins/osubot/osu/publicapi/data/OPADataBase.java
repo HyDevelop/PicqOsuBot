458c45bbe926db59f958c625598af914ff0ac992
Hykilpikonna <admin@moecraft.cc>
2018-08-11 12:06:23 +0800

    [+] OPA数据父类

---
+++ src/main/java/cc/moecraft/icq/plugins/osubot/osu/publicapi/data/OPADataBase.java
@@ -0,0 +1,14 @@
+package cc.moecraft.icq.plugins.osubot.osu.publicapi.data;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/04/24 创建!
+ * Created by Hykilpikonna on 2018/04/24!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ */
+public abstract class OPADataBase
+{
+    // 这里的DataBase真的只是指Data类的Base...
+    // 像Parameter的ParameterBase一样...
+    // 不是数据库
+}
