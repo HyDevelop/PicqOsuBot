f84119e2498bfebc9aad024e6d8fb5e55f33f9b4
Hykilpikonna <admin@moecraft.cc>
2018-08-11 12:07:19 +0800

    [+] OPA参数注解

---
+++ src/main/java/cc/moecraft/icq/plugins/osubot/osu/publicapi/parameters/tags/OPAParameter.java
@@ -0,0 +1,16 @@
+package cc.moecraft.icq.plugins.osubot.osu.publicapi.parameters.tags;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/04/24 创建!
+ * Created by Hykilpikonna on 2018/04/24!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OPAParameter
+{
+    boolean required();
+}
