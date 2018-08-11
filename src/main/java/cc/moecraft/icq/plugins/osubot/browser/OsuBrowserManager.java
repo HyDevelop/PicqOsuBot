29c3f7a417fdb57fc1bfd77a2941c73cb02b5309
Hykilpikonna <admin@moecraft.cc>
2018-08-11 12:19:59 +0800

    [+] 浏览器管理器

---
+++ src/main/java/cc/moecraft/icq/plugins/osubot/browser/OsuBrowserManager.java
@@ -0,0 +1,18 @@
+package cc.moecraft.icq.plugins.osubot.browser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/08 创建!
+ * Created by Hykilpikonna on 2018/08/08!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class OsuBrowserManager
+{
+    private Map<Class<? extends OsuBrowser>, ArrayList<OsuBrowser>> browsers = new HashMap<>();
+}
