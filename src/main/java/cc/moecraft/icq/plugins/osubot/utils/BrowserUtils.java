dc8f5e88e6ca1cd5bf3cebe9ddb7f6064c9572e3
Hykilpikonna <admin@moecraft.cc>
2018-08-11 12:05:40 +0800

    [+] 浏览器工具类

---
+++ src/main/java/cc/moecraft/icq/plugins/osubot/utils/BrowserUtils.java
@@ -0,0 +1,22 @@
+package cc.moecraft.icq.plugins.osubot.utils;
+
+import com.teamdev.jxbrowser.chromium.swing.BrowserView;
+import com.teamdev.jxbrowser.chromium.swing.internal.LightWeightWidget;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/10 创建!
+ * Created by Hykilpikonna on 2018/08/10!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class BrowserUtils
+{
+}
