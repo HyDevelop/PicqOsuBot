926482104d2f16bb8b4ca85ee00f5a39308be8de
Hykilpikonna <admin@moecraft.cc>
2018-08-11 12:15:29 +0800

    [+] Osu浏览器抽象类封装

---
+++ src/main/java/cc/moecraft/icq/plugins/osubot/browser/OsuBrowser.java
@@ -0,0 +1,25 @@
+package cc.moecraft.icq.plugins.osubot.browser;
+
+import cc.moecraft.icq.plugins.osubot.utils.BrowserUtils;
+import cc.moecraft.icq.plugins.osubot.utils.ResourceFileUtils;
+import cc.moecraft.utils.ReflectUtils;
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.BrowserType;
+import com.teamdev.jxbrowser.chromium.swing.BrowserView;
+import lombok.Getter;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/10 创建!
+ * Created by Hykilpikonna on 2018/08/10!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public abstract class OsuBrowser
+{
+}
