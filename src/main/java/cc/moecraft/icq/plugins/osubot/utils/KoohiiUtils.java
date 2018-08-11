11e7e7d868a1d73f022a63824418d77c741aac4f
Hykilpikonna <admin@moecraft.cc>
2018-08-11 12:13:19 +0800

    [+] Koohii工具类

---
+++ src/main/java/cc/moecraft/icq/plugins/osubot/utils/KoohiiUtils.java
@@ -0,0 +1,26 @@
+package cc.moecraft.icq.plugins.osubot.utils;
+
+import cc.moecraft.icq.plugins.osubot.osu.Mode;
+import cc.moecraft.icq.plugins.osubot.osu.Mods;
+import cc.moecraft.icq.plugins.osubot.osu.publicapi.data.OPAUserRecentData;
+import cn.hutool.http.HttpUtil;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+import static com.github.francesco149.koohii.Koohii.*;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/11 创建!
+ * Created by Hykilpikonna on 2018/08/11!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class KoohiiUtils
+{
+}
