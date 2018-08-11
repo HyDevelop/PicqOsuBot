7986a743bc8e013115a90fc0cced39ab87b6fe2c
Hykilpikonna <admin@moecraft.cc>
2018-08-11 12:00:04 +0800

    [+] 字符串工具类

---
+++ src/main/java/cc/moecraft/icq/plugins/osubot/utils/StringUtils.java
@@ -0,0 +1,13 @@
+package cc.moecraft.icq.plugins.osubot.utils;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/10 创建!
+ * Created by Hykilpikonna on 2018/08/10!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class StringUtils
+{
+}
