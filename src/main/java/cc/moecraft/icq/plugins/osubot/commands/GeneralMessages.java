d8cf8d2f12fcaced5c5ad65a82ccf60681206c72
Hykilpikonna <admin@moecraft.cc>
2018-08-11 12:08:02 +0800

    [+] 通用消息类

---
+++ src/main/java/cc/moecraft/icq/plugins/osubot/commands/GeneralMessages.java
@@ -0,0 +1,15 @@
+package cc.moecraft.icq.plugins.osubot.commands;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/10 创建!
+ * Created by Hykilpikonna on 2018/08/10!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+class GeneralMessages
+{
+    static String FAILED_NULL = "查询失败, API返回异常... 请重试w";
+
+}
