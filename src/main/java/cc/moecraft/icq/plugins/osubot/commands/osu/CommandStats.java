a82a8a4910efd6b47169db8db7927fe173244131
Hykilpikonna <admin@moecraft.cc>
2018-08-24 05:39:07 -0400

    [U] 更新Stats指令

---
+++ src/main/java/cc/moecraft/icq/plugins/osubot/commands/osu/CommandStats.java
@@ -0,0 +1,59 @@
+package cc.moecraft.icq.plugins.osubot.commands.osu;
+
+import cc.moecraft.icq.command.CommandProperties;
+import cc.moecraft.icq.event.events.message.EventMessage;
+import cc.moecraft.icq.plugins.osubot.Main;
+import cc.moecraft.icq.plugins.osubot.browser.browsers.StatsBrowser;
+import cc.moecraft.icq.plugins.osubot.commands.OsuCommandBase;
+import cc.moecraft.icq.plugins.osubot.osu.exceptions.UserNotFoundException;
+import cc.moecraft.icq.plugins.osubot.osu.webapi.OWAUserData;
+import cc.moecraft.icq.plugins.osubot.osu.webapi.OWAUtils;
+import cc.moecraft.icq.user.User;
+import cc.moecraft.utils.ArrayUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/07 创建!
+ * Created by Hykilpikonna on 2018/08/07!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class CommandStats extends OsuCommandBase
+{
+    public CommandStats()
+    {
+        super(1, "stats");
+    }
+
+    @Override
+    public String runOsu(EventMessage event, User user, String command, ArrayList<String> args) throws UserNotFoundException, IOException
+    {
+        event.respond("正在查询... 请稍后ww");
+        long startTime = System.currentTimeMillis();
+
+        String username = ArrayUtils.getTheRestArgsAsString(args, 0);
+        OWAUserData userData = OWAUtils.getUserData(username);
+        Long userId = userData.getId();
+
+        File imageFile = Main.getInstance().getBrowserManager().getBrowser(StatsBrowser.class).render(userId);
+        logComplete(user.getInfo().getNickname(), username, startTime);
+        return getImageMessage(imageFile);
+    }
+
+    @Override
+    public String help(String command)
+    {
+        return "用法: /" + command + " [用户名]";
+    }
+
+    @Override
+    public CommandProperties properties()
+    {
+        return new CommandProperties("stats", "me", "home", "stat");
+    }
+}
