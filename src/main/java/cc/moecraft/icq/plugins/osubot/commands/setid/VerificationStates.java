b26e797f786c59b34d23556ea2f100a1ad09a6cf
Hykilpikonna <admin@moecraft.cc>
2018-09-03 09:46:50 -0400

    [+] 添加验证状态常量类

---
+++ src/main/java/cc/moecraft/icq/plugins/osubot/commands/setid/VerificationStates.java
@@ -0,0 +1,15 @@
+package cc.moecraft.icq.plugins.osubot.commands.setid;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/09/02 创建!
+ * Created by Hykilpikonna on 2018/09/02!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class VerificationStates
+{
+    public static final String VERIFYING = "verifying";
+    public static final String VERIFIED = "verified";
+}
