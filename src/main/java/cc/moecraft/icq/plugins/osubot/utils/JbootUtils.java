d77f42a7b5d947136a54ff7e3b700bd52a8a7b2e
Hykilpikonna <admin@moecraft.cc>
2018-09-03 10:21:16 -0400

    [+] Jboot工具类

---
+++ src/main/java/cc/moecraft/icq/plugins/osubot/utils/JbootUtils.java
@@ -0,0 +1,25 @@
+package cc.moecraft.icq.plugins.osubot.utils;
+
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.core.JFinal;
+import io.jboot.Jboot;
+import io.jboot.web.JbootAppConfig;
+import lombok.Getter;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/09/02 创建!
+ * Created by Hykilpikonna on 2018/09/02!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class JbootUtils
+{
+}
