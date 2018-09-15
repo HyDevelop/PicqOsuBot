646b33687e20f520dcbec08a38f4ef6c5e3ada3c
Hykilpikonna <admin@moecraft.cc>
2018-09-15 18:01:56 -0400

    [+] Image转BufferedImage方法

--- src/main/java/cc/moecraft/icq/plugins/osubot/utils/ImageUtils.java
+++ src/main/java/cc/moecraft/icq/plugins/osubot/utils/ImageUtils.java
@@ -47,4 +47,28 @@ public class ImageUtils
 
         return result;
     }
+
+    /**
+     * Converts a given Image into a BufferedImage
+     *
+     * @param img The Image to be converted
+     * @return The converted BufferedImage
+     *
+     * @author @Sri Harsha Chilakapati
+     */
+    public static BufferedImage toBufferedImage(Image img)
+    {
+        if (img instanceof BufferedImage) return (BufferedImage) img;
+
+        // Create a buffered image with transparency
+        BufferedImage bufferedImage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+        // Draw the image on to the buffered image
+        Graphics2D bGr = bufferedImage.createGraphics();
+        bGr.drawImage(img, 0, 0, null);
+        bGr.dispose();
+
+        // Return the buffered image
+        return bufferedImage;
+    }
 }
