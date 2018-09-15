2c595cd898e9bea6b60981de059a5bf075abeb5b
Hykilpikonna <admin@moecraft.cc>
2018-09-15 18:01:08 -0400

    [+] 图片工具类

---
+++ src/main/java/cc/moecraft/icq/plugins/osubot/utils/ImageUtils.java
@@ -0,0 +1,16 @@
+package cc.moecraft.icq.plugins.osubot.utils;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/07 创建!
+ * Created by Hykilpikonna on 2018/08/07!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class ImageUtils
+{
+}
