ef5ec62b0bed33e556c434ff92a72c7d1393b451
Hykilpikonna <admin@moecraft.cc>
2018-09-21 18:22:23 -0400

    [+] 当前页不能为0

--- src/main/java/cc/moecraft/icq/plugins/osubot/browser/browsers/RankingBrowser.java
+++ src/main/java/cc/moecraft/icq/plugins/osubot/browser/browsers/RankingBrowser.java
@@ -69,6 +69,7 @@ public class RankingBrowser extends OsuBrowser
         // 分页
         int maxPage = userDataList.size() / entriesPerPage;
         if (maxPage < 1) maxPage = 1;
+        if (page < 1) page = 1;
     }
 
     public File render(Group group, ArrayList<UserData> userDataList, int modeCode, PageInfo page) throws IOException
